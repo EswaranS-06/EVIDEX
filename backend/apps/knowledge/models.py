7c26db2c90d648f51a9c6085db3c301bd49e85b0
Eswaran-S06 <eswaransk06@gmail.com>
2026-01-24 20:40:26 +0530

    adding 'status column to report model

--- backend/apps/knowledge/models.py
+++ backend/apps/knowledge/models.py
@@ -127,7 +127,16 @@ class VulnerabilityDefinition(models.Model):
 from django.db import models
 from django.contrib.auth.models import User
 
+# apps/knowledge/models.py
+
 class Report(models.Model):
+    STATUS_CHOICES = [
+        ("Draft", "Draft"),
+        ("In Progress", "In Progress"),
+        ("Completed", "Completed"),
+        ("Verified", "Verified"),
+    ]
+
     client_name = models.CharField(max_length=200)
     application_name = models.CharField(max_length=200)
     report_type = models.CharField(max_length=100)
@@ -136,6 +145,12 @@ class Report(models.Model):
     end_date = models.DateField(null=True, blank=True)
 
     prepared_by = models.CharField(max_length=150)
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="Draft",
+    )
 
     created_by = models.ForeignKey(
         User,
@@ -148,6 +163,8 @@ class Report(models.Model):
 
     def __str__(self):
         return f"{self.client_name} - {self.application_name}"
+
+
 
 from apps.knowledge.models import VulnerabilityDefinition  
 
